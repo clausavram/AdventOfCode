929e8e63e00fb9a1ccd93c99b660b0b6496daf4c
Claudiu Avram <claudiu.avram@tora.com>
2021-12-11 23:31:35 +0200

    2021-day1 - solved

---
+++ src/main/kotlin/y2021/Day1.kt
@@ -0,0 +1,37 @@
+package y2021
+
+import java.io.File
+
+class Day1
+
+fun main() {
+    val lines = File(Day1::class.java.getResource("day1.input.txt")!!.toURI()).readLines()
+    val numbers = lines.map { Integer.valueOf(it) }
+    partOne(numbers)
+    partTwo(numbers)
+}
+
+private fun partOne(numbers: List<Int>) {
+    var prevNumber = numbers[0]
+    var depthIncreases = 0
+    for (number in numbers) {
+        if (number > prevNumber) {
+            depthIncreases++
+        }
+        prevNumber = number
+    }
+    println("Depth increases: $depthIncreases")
+}
+
+private fun partTwo(numbers: List<Int>) {
+    var prevSum = numbers[0] + numbers[1] + numbers[2]
+    var depthIncreases = 0
+    for (crtIdx in 3 until numbers.size) {
+        val crtSum = prevSum - numbers[crtIdx - 3] + numbers[crtIdx]
+        if (crtSum > prevSum) {
+            depthIncreases++
+        }
+        prevSum = crtSum
+    }
+    println("3-measurement sliding window depth increases: $depthIncreases")
+}
