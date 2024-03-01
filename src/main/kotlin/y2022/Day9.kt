52722165265ec6776f8dcf6aabebb53d9075c116
Claudiu Avram <claudiu.avram@tora.com>
2024-03-01 11:48:03 +0200

    2022-day9 - attempt at part 2

--- src/main/kotlin/y2022/Day9.kt
+++ src/main/kotlin/y2022/Day9.kt
@@ -3,14 +3,16 @@ package y2022
 import util.FileType
 import util.Point2D
 import util.getFile
+import kotlin.math.abs
 import kotlin.math.max
 import kotlin.math.min
+import kotlin.math.sign
 
-private enum class Direction(val str: String) {
-    Left("L"),
-    Right("R"),
-    Up("U"),
-    Down("D");
+private enum class Direction(val str: String, val vector: Point2D) {
+    Left("L", Point2D(-1, 0)),
+    Right("R", Point2D(1, 0)),
+    Up("U", Point2D(0, 1)),
+    Down("D", Point2D(0, -1));
 
     companion object {
         val strToDir = values().associateBy { it.str }
@@ -20,11 +22,14 @@ private enum class Direction(val str: String) {
     override fun toString() = str
 }
 
-private data class Movement(val direction: Direction, val distance: Int) {
+private data class Movement(val direction: Direction, val distance: Int, val str: String) {
+    val vector
+        get() = direction.vector
+
     companion object {
         fun fromStr(str: String): Movement {
             val (dirStr, distStr) = str.split(" ")
-            return Movement(Direction.fromStr(dirStr), distStr.toInt())
+            return Movement(Direction.fromStr(dirStr), distStr.toInt(), dirStr)
         }
     }
 }
@@ -33,50 +38,84 @@ fun main() {
     val file = getFile(object {}, FileType.EXAMPLE)
     val moves = file.readLines().map { Movement.fromStr(it) }
 
-    var head = Point2D(0, 0)
-    var tail = head
-    val tailHistory = LinkedHashSet<Point2D>()
-    tailHistory += tail
+    solve("Part 1", 2, moves)
+//    solve("Part 2", 10, moves)
+}
 
-    for (move in moves) {
-        for (i in 1..move.distance) {
-            when (move.direction) {
-                Direction.Left -> {
-                    head += Point2D(-1, 0)
-                    if (tail.x - head.x > 1) {
-                        tail = Point2D(head.x + 1, head.y)
-                    }
+private fun solve(problemPart: String, ropeLength: Int, moves: List<Movement>) {
+    // part 2
+    val knots = Array(ropeLength) { Point2D(0, 0) }
+    val endTailHistory = LinkedHashSet(setOf(Point2D(0, 0)))
+    println("== Initial State ==")
+    printRope(knots, -11..14, -5..15)
+    for (headMove in moves) {
+
+        for (i in 1..headMove.distance) {
+            knots[0] += headMove.vector
+
+            for (crtKnotIdx in 1 until knots.size) {
+                val oldTail = knots[crtKnotIdx]
+                val newHead = knots[crtKnotIdx - 1]
+                val newTail = advanceTail(oldTail, newHead)
+
+                if (oldTail == newTail) {
+                    break // if this knot didn't move, so the ones after it won't as well
                 }
 
-                Direction.Right -> {
-                    head += Point2D(1, 0)
-                    if (head.x - tail.x > 1) {
-                        tail = Point2D(head.x - 1, head.y)
+                knots[crtKnotIdx] = newTail
+            }
+            endTailHistory += knots.last()
+        }
+        // start-row:90, start-col:11   total-rows:424, total-cols:195
+        println("== ${headMove.direction} ${headMove.distance} ==")
+        printRope(knots, -11..14, -5..15)
+//        printRope(knots, -89..14, -5..15)
+    }
+
+    printTailTrace(endTailHistory)
+    println("${problemPart}: 10-knot rope's tail visited ${endTailHistory.size} positions")
+}
+
+private fun advanceTail(oldTail: Point2D, newHead: Point2D): Point2D {
+    val (xDist, yDist) = newHead - oldTail
+    return when {
+        abs(xDist) > 1 -> Point2D(newHead.x - xDist.sign, newHead.y) // move horizontally
+        abs(yDist) > 1 -> Point2D(newHead.x, newHead.y - yDist.sign) // move vertically
+        else -> oldTail // don't move tail
+    }
+}
+
+private fun printRope(knots: Array<Point2D>, xRange: IntRange, yRange: IntProgression) {
+    println()
+    for (y in yRange.reversed()) {
+        for (x in xRange) {
+            val crtPoint = Point2D(x, y)
+            if (crtPoint == knots[0]) print('H')
+            else {
+                var matchedAnyKnot = false
+                for (crtKnotIdx in 1 until knots.size) {
+                    if (crtPoint == knots[crtKnotIdx]) {
+                        print(crtKnotIdx)
+                        matchedAnyKnot = true
+                        break
                     }
                 }
-
-                Direction.Up -> {
-                    head += Point2D(0, 1)
-                    if (head.y - tail.y > 1) {
-                        tail = Point2D(head.x, head.y - 1)
-                    }
-                }
-
-                Direction.Down -> {
-                    head += Point2D(0, -1)
-                    if (tail.y - head.y > 1) {
-                        tail = Point2D(head.x, head.y + 1)
+                if (!matchedAnyKnot) {
+                    if (x == 0 && y == 0) {
+                        print('s')
+                    } else {
+                        print('.')
                     }
                 }
             }
-            tailHistory += tail
         }
+        println()
     }
-    printTailTrace(tailHistory, head, tail)
-    println("Part 1: ${tailHistory.size}")
+    println()
 }
 
-private fun printTailTrace(visitedPoints: LinkedHashSet<Point2D>, head: Point2D, tail: Point2D) {
+private fun printTailTrace(visitedPoints: LinkedHashSet<Point2D>) {
+    println("== Tail trace:")
     var (xMin, xMax, yMin, yMax) = listOf(0, 0, 0, 0)
     visitedPoints.forEach {
         xMin = min(xMin, it.x)
@@ -88,9 +127,7 @@ private fun printTailTrace(visitedPoints: LinkedHashSet<Point2D>, head: Point2D,
     for (y in yMax downTo yMin) {
         for (x in xMin..xMax) {
             val crtPoint = Point2D(x, y)
-            if (crtPoint == head) print('H')
-            else if (crtPoint == tail) print('T')
-            else if (x == 0 && y == 0) print('s')
+            if (x == 0 && y == 0) print('s')
             else if (crtPoint in visitedPoints) print('X')
             else print('.')
         }
